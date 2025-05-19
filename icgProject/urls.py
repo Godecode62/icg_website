24b8c2734858da598a500db3a7914cf7afe0bbb1
Godecode62 <kikoyolo63@gmail.com>
2025-05-20 00:03:29 +0200

    Suppression de l'url de l'admin

--- icgProject/urls.py
+++ icgProject/urls.py
@@ -22,7 +22,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('',include('appli.urls')),
 ]
 
