bb99babdf83b348b1746591dd98d101c0750752b
Godecode62 <kikoyolo63@gmail.com>
2025-05-21 00:34:20 +0200

    essaie

--- icgProject/settings.py
+++ icgProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [""]
+ALLOWED_HOSTS = ["icguinea.com",'www.icguinea.com','icg-6bg2.onrender.com']
 
 
 import os
