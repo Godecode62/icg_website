6457287d037c700c168b5d6673329b436de05938
Godecode62 <kikoyolo63@gmail.com>
2025-05-21 09:00:34 +0200

    configuration de amazon s3

--- icgProject/settings.py
+++ icgProject/settings.py
@@ -110,6 +110,19 @@ DATABASES = {
     'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
+#AWS
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'icg-burcket'
+AWS_s3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -174,5 +187,4 @@ SESSION_COOKIE_SECURE = True
 # Protection contre les attaques XSS/Clickjacking
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
-
 
